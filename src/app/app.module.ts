f66c797a98e75d7b1460ece2b203aad3a31e7eb1
Ligory <>
2019-03-18 17:40:16 +0530

    feature module intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { FeatureModule } from './modules/feature.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, SharedModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, SharedModule, FeatureModule, AppRoutingModule],
   declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
   providers: [],
   bootstrap: [AppComponent]
